ca5fc5720c1636b94d6314e8a770bd3bc81d6d42
Paulo Santana <paulo.santana.r@gmail.com>
2022-05-08 14:51:01 -0300

    style: reindent iterator_base.hpp

--- src/internals/iterator_base.hpp
+++ src/internals/iterator_base.hpp
@@ -4,27 +4,22 @@
 
 #include <cstddef>
 namespace ft {
-    struct random_access_iterator_tag {};
-    struct bidirectional_iterator_tag {};
-    struct forward_iterator_tag {};
-    struct input_iterator_tag {};
-    struct output_iterator_tag {};
 
-    template <
-        typename Category,
-        typename T,
-        typename Distance = std::ptrdiff_t,
-        typename Pointer = T*,
-        typename Reference = T&
-        >
-    struct iterator
-    {
-        typedef T value_type;
-        typedef Pointer pointer;
-        typedef Reference reference;
-        typedef Category iterator_category;
-        typedef Distance difference_type;
-    };
+template <
+    typename Category,
+    typename T,
+    typename Distance = std::ptrdiff_t,
+    typename Pointer = T*,
+    typename Reference = T&
+    >
+struct iterator
+{
+    typedef T value_type;
+    typedef Pointer pointer;
+    typedef Reference reference;
+    typedef Category iterator_category;
+    typedef Distance difference_type;
+};
 
 }
 
