5524b1bb96dc8c7dcc6a413a1e2bbae0ddc462d9
Paulo Santana <paulo.santana.r@gmail.com>
2022-05-25 23:52:58 -0300

    feat: implement ft::lexicographical_compare

---
+++ src/lexicographical_compare.hpp
@@ -0,0 +1,22 @@
+#ifndef LEXICOGRAPHICAL_COMPARE_HPP
+#define LEXICOGRAPHICAL_COMPARE_HPP
+
+namespace ft {
+
+template <typename Iter1, typename Iter2>
+bool lexicographical_compare(
+        Iter1 first1, Iter1 last1,
+        Iter2 first2, Iter2 last2) {
+
+    while (first1 != last1 && first2 != last2) {
+        if (*first1 < *first2) return true;
+        else if (*first2 < *first1) return false;
+        ++first1;
+        ++first2;
+    }
+    return (first1 == last1 && first2 != last2);
+}
+
+}
+
+#endif // !LEXICOGRAPHICAL_COMPARE_HPP
