93d74278b2d50f26c8e19396a29a3de22a735634
Paulo Santana <paulo.santana.r@gmail.com>
2022-05-26 20:45:50 -0300

    test: stack wrapper constructor

--- tests/stack.test.cpp
+++ tests/stack.test.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <stack>
 #include "test_utils.hpp"
 
@@ -16,7 +17,17 @@ static void test_stack_default_constructor() {
 
 }
 
+static void test_stack_wrapper_constructor() {
+    println("test stack default constructor");
+
+    ft::vector<int> vec(3, 2);
+    ft::stack<int, ft::vector<int> > stack(vec);
+
+    std::cout << "stack.size(): " << stack.size() << std::endl;
+}
+
 void testStack() {
     test_stack_default_constructor();
+    test_stack_wrapper_constructor();
 
 }
