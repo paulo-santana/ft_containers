14ce6af3988792cdad1af306512f66808589bd6d
Paulo Santana <paulo.santana.r@gmail.com>
2022-06-10 19:09:20 -0300

    test: tree's last element

--- main.cpp
+++ main.cpp
@@ -19,7 +19,29 @@ int main (void)
     else
         std::cerr << "FT containers" << std::endl;
 
-
+    std::map<int, int> map;
+    for (int i = 333; i < 343; i++) {
+        map[i] = i * i;
+    }
+    std::map<int, int>::iterator it = map.begin();
+    for (int i = 0; i < 15; i++) {
+        std::cout << "*it-- -> " << (*it--).first << std::endl;
+    }
+    std::_Rb_tree_const_iterator<std::pair<const int, int> > const_iter = it;
+    // if (it == const_iter) {
+    //     // return 1;
+    // }
+    //
+    /*
+     * if (hint >= current)
+     *     nope
+     * else if (hint.successor() < current)
+     *     nope
+     * else 
+     *     insert at right of hint
+     * */
+    std::_Rb_tree_iterator<std::pair<const int, int> > casted = const_iter._M_const_cast();
+    (void)casted;
     testVectorIterator();
     testReverseIterator();
     testVector();
