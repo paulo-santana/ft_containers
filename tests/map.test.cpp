8dce68fafa2ee436b6cb089271fe9aec8aabb373
Paulo Santana <paulo.santana.r@gmail.com>
2022-06-19 17:29:19 -0300

    test: add a cplusplus test to the map operator[]

--- tests/map.test.cpp
+++ tests/map.test.cpp
@@ -247,6 +247,22 @@ static void test_access_operator() {
 
 }
 
+static void test_access_operator_cplusplus() {
+
+  ft::map<char,std::string> mymap;
+
+  mymap['a']="an element";
+  mymap['b']="another element";
+  mymap['c']=mymap['b'];
+
+  std::cout << "mymap['a'] is " << mymap['a'] << '\n';
+  std::cout << "mymap['b'] is " << mymap['b'] << '\n';
+  std::cout << "mymap['c'] is " << mymap['c'] << '\n';
+  std::cout << "mymap['d'] is " << mymap['d'] << '\n';
+
+  std::cout << "mymap now contains " << mymap.size() << " elements.\n";
+}
+
 void testMap() {
     test_empty_constructor();
     test_range_constructor();
@@ -261,4 +277,5 @@ void testMap() {
     test_size();
     test_max_size();
     test_access_operator();
+    test_access_operator_cplusplus();
 }
