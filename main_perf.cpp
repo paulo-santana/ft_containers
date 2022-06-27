0f08abfd642e4fa14f3c1d2808672302d741737f
Paulo Santana <paulo.santana.r@gmail.com>
2022-06-26 23:19:45 -0300

    feat(perf): test vector fill constructor

--- main_perf.cpp
+++ main_perf.cpp
@@ -1,13 +1,57 @@
 #include "Timer.hpp"
+#include "vector.hpp"
 #include <iostream>
+#include <vector>
+
+#define NORMAL "\x1B[0m"
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+
+Timer timer;
+
+static void print_times(long elements, double ft_seconds, double std_seconds) {
+    std::cout << "ft::vector("<< elements << "): " << ft_seconds << std::endl;
+    std::cout << "std::vector(" << elements << "): " << std_seconds << std::endl;
+
+    if (ft_seconds > std_seconds) {
+        double slower = ft_seconds / std_seconds;
+        std::cout << "ft is ";
+        std::string color = NORMAL;
+        if (slower >= 20)
+            color = RED;
+        std::cout << color << slower << "x slower" << NORMAL << std::endl;
+    } else {
+        double faster = std_seconds / ft_seconds;
+        std::cout << GREEN << "ft is " << faster << "x faster" << NORMAL << std::endl;
+    }
+}
+
+static void test_vector_fill_constructor() {
+    double ft_seconds, std_seconds;
+    long n = 30000000;
+
+    timer.reset();
+    ft::vector<int> intvec(n);
+    ft_seconds = timer.elapsed_seconds();
+
+    timer.reset();
+    std::vector<int> std_intvec(n);
+    std_seconds = timer.elapsed_seconds();
+
+    print_times(n, ft_seconds, std_seconds);
+
+    timer.reset();
+    ft::vector<std::string> string_vec(n, std::string("default"));
+    ft_seconds = timer.elapsed_seconds();
+
+    timer.reset();
+    std::vector<std::string> std_string_vec(n, std::string("default"));
+    std_seconds = timer.elapsed_seconds();
+
+    print_times(n, ft_seconds, std_seconds);
+}
 
 int main(void) {
-    Timer timer;
-    timer.reset();
-    for (int i = 0; i < 4000000; i++) {
-        std::cout << "adjfkadlsfjaklsdjfkas";
-    }
-    std::cout << "adjf" << std::endl;
-    double time = timer.elapsed_seconds();
-    std::cout << "elapsed time: " << time << std::endl;
+
+    test_vector_fill_constructor();
 }
